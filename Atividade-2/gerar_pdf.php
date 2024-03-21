43a80cf07f0a637fadab34acef426383dd8f6a76
Gustavo Henriky Ferreira Bernardes <gustavo.bernardes@unidavi.edu.br>
2024-03-21 19:12:27 -0300

    .

--- Atividade-2/gerar_pdf.php
+++ Atividade-2/gerar_pdf.php
@@ -2,15 +2,14 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Crie um novo objeto mPDF
 $mpdf = new \Mpdf\Mpdf();
 
-// HTML que você deseja converter em PDF
+
 $html = '<h1>Meu Primeiro PDF com mPDF</h1>
 <p>Este é um exemplo simples de como gerar um PDF usando mPDF.</p>';
 
-// Adicione o HTML ao PDF
+
 $mpdf->WriteHTML($html);
 
-// Saída do PDF
+
 $mpdf->Output('meu_pdf.pdf', 'D');
